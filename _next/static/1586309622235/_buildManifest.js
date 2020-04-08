0ca316f0c207afc09fba43a74fb08813063bdfb7
EthanBelcher <54596488+EthanBelcher@users.noreply.github.com>
2020-04-08 12:15:10 -0700

    Update _buildManifest.js

--- _next/static/1586309622235/_buildManifest.js
+++ _next/static/1586309622235/_buildManifest.js
@@ -1,1 +1,8 @@
-
+self._BUILD_MANIFEST = (function(a) {
+  return {
+    "/": [a],
+    "/index": [a],
+    "/xo/[album]": [a]
+  }
+}("static\u002Fchunks\u002F29da6873ac922503f5298794a05160fe3f2aeff8.3941bc26fd9b8ffc269c.js"));
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
